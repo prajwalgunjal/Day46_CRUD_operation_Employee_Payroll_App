3a80256943979ac24a5e878835b355060fea20fe
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-27 17:49:23 +0530

    uc1 added

---
+++ js/new_home.js
@@ -0,0 +1,92 @@
+let empPayrollList;
+window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent=empPayrollList.length;
+    createInnerHtml();
+});
+
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('employeePayrollList') ? 
+                        JSON.parse(localStorage.getItem('employeePayrollList')) : [];
+}
+
+const createInnerHtml = () => {
+    if(empPayrollList.length==0) return;
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" + 
+    "<th>Salary</th><th>Start Date</th><th>Actions</th>";               
+    let innerHtml = `${headerHtml}`;
+    // let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList) {                    
+    innerHtml = `${innerHtml}
+     <tr>
+            <td><img class="profile" alt="" src="${empPayrollData._profilePic}  "> 
+            </td> 
+            <td>${empPayrollData._name}</td> 
+            <td>${empPayrollData._gender}</td>
+            <td>${getDeptHtml(empPayrollData._department)}</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${stringifyDate(empPayrollData._startDate)}</td>
+            <td> 
+            <img id="${empPayrollData._id}" onclick="remove(this)" 
+            alt="delete"src="./assets/icons/delete-black-18dp.svg">
+            <img id="${empPayrollData._id}" onclick="update(this)"
+            src="./assets/icons/create-black-18dp.svg" alt="edit">
+          </td> 
+        </tr> 
+`;
+    }
+document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal =[
+        {
+            _name: 'prajwal gunjal',
+            _gender: 'male',
+            _department: [
+                'Engineering',
+                'Finance'
+            ],
+            _salary:'500000',
+            _startDate: '29 Oct 2019',
+            _note:'',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile-images/Ellipse -2.png'
+        },
+        {
+            _name: 'arpit patil',
+            _gender: 'Female',
+            _department: [
+                'Finance'
+            ],
+            _salary:'5000000',
+            _startDate: '29 Sept 2020',
+            _note:'',
+            _id: new Date().getTime() +1,
+            _profilePic: '../assets/profile-images/Ellipse -2.png'
+        }
+    ];
+    return empPayrollListLocal;
+}
+
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList){
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
+
+
+const remove = (node) => {
+    let empPayrollData =empPayrollList.find(empData => empData._id == node._id);
+    if(!empPayrollData) return;
+    const index = empPayrollList
+                .map(empData => empData._id)
+                .indexOf(empPayrollData._id);
+    empPayrollList.splice(index,1);
+    localStorage.setItem("EmployeePayrollList",JSON.stringify(empPayrollList));
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
+}
---
+++ js/site_properties.js
@@ -0,0 +1,5 @@
+
+let site_properties = {
+    home_page : "new_home.html",
+    add_emp_payroll_page: "new_payroll_form.html"
+}; 
